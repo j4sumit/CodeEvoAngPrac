613560709073c227efc09ff488894bad67a77e3b
Sumit Kumar <j4sumit@gmail.com>
2024-05-20 16:26:55 +0530

    6 - Property Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,12 +6,15 @@ import { Component, OnInit } from '@angular/core';
     <h1>
      Welcome Sumit {{sirname}}
     </h1>
+    <input type="text" value="Chaurasiya" [disabled]="status">// propery binding accept boolean value also
+    <input type="text" value="Chaurasiya" disabled={status}> //Interpolation only accept string and nnumber
   `,
   styles: [
   ]
 })
 export class TestComponent implements OnInit {
-public sirname="Chauasiya"
+public sirname="Chauasiya";
+public status =false;
   constructor() { }
 
   ngOnInit(): void {
